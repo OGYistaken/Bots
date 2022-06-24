be98c4b82c85b69d9bd02c8932bfd8ae971ee7b8
quaofGOD <69598671+quaofGOD@users.noreply.github.com>
2022-06-25 01:28:49 +0300

    as

--- websockerv3.py
+++ websockerv3.py
@@ -42,12 +42,17 @@ def bros() :
     element = chrome.find_element(By.XPATH, '//*[@id="vote-button"]') 
     element.click();
     time.sleep(5)
-    WebDriverWait(chrome, 10).until(EC.frame_to_be_available_and_switch_to_it((By.CSS_SELECTOR,"iframe[name^='a-'][src^='https://www.google.com/recaptcha/api2/anchor?']")))
-    WebDriverWait(chrome, 10).until(EC.element_to_be_clickable((By.XPATH, "//span[@id='recaptcha-anchor']"))).click()
+    iframes = chrome.find_elements_by_tag_name("iframe")
+    chrome.switch_to.frame(chrome.find_elements_by_tag_name("iframe")[2])
+    check_box = WebDriverWait(chrome, 10).until(EC.element_to_be_clickable((By.ID ,"recaptcha-anchor")))
+    check_box.click()
     time.sleep(15)
-    chrome.switch_to.default_content()
-    WebDriverWait(chrome, 10).until(EC.frame_to_be_available_and_switch_to_it((By.CSS_SELECTOR,"iframe[name^='a-'][src^='https://www.google.com/recaptcha/api2/bframe?hl=en&v=M10Y1otwqRkBioiFUKRQ8s3N&k=6LfDlbAbAAAAALJvKffc-P-uv5EVxTN9gdyO1O4x']")))
-    WebDriverWait(chrome, 20).until(EC.element_to_be_clickable((By.XPATH, "//button[@id='solver-button']"))).click() 
+   # iframes = chrome.find_elements_by_tag_name("iframe")
+    #chrome.switch_to.frame(chrome.find_elements_by_xpath("/html/body/div[6]/div[4]/iframe"))
+    #capt_btn = WebDriverWait(chrome, 50).until(
+     #          EC.element_to_be_clickable((By.XPATH ,'//button[@id="solver-button"]'))
+    #)
+    #capt_btn.click()
     print("Tıkladım v2")
     time.sleep(500)
     #//*[@id="solver-button"]  /html/body/div[7]/div[4]/iframe
